3d9c28f020b37669fb59625dcc64845d07d978db
iRusev90 <ivan.rusev90@gmail.com>
2015-09-03 00:16:54 +0300

    added a few categories
    created a select option for product categories and fixed a minor naming
    mistake of a file

--- crudeExampleOfAddingItem/tableGenerator.js
+++ crudeExampleOfAddingItem/tableGenerator.js
@@ -4,8 +4,17 @@ function generateTable (argument) {
 	nameRow,
 	priceRow,
 	descriptionRow,
-	imageURLRow;
+	imageURLRow,
+	categoryRow,
+	categorySelector,
+	nameInput,
+	descriptionInput,
+	priceInput,
+	imageURLInput,
+	submitButton;
     
+
+    //rows for the table
     tableWrapper = $('<div/>');
 	table = $("<table/>");
 	nameRow = $("<tr/>");
@@ -15,23 +24,38 @@ function generateTable (argument) {
 	categoryRow = $("<tr/>");
 	submitRow = $('<tr/>');
 
-
-	var nameInput = $('<input/>'),
-	    descriptionInput = $('<input/>'),
-	    priceInput = $('<input/>'),
-	    imageURLInput = $('<input/>'),
-	    submitButton = $('<button/>');
+    //inputs for the second tds in the rows
+	nameInput = $('<input/>'),
+	descriptionInput = $('<input/>'),
+	priceInput = $('<input/>'),
+	imageURLInput = $('<input/>'),
+	submitButton = $('<button/>');
+	categorySelector = $('<select/>');
     
+    //adding ids
     tableWrapper.attr('id','productInputForm');
     tableWrapper.attr('class','afterLogIn');
 	nameInput.attr('id','productName');
 	descriptionInput.attr('id','productDescription');
 	priceInput.attr('id','productPrice');
 	imageURLInput.attr('id','productURL');
+	categorySelector.attr('id','productCategory')
 	submitButton.attr('id','addItem');
 	submitButton.attr('style','width:90%');
 	submitButton.text("Add product");
+    
+    //adding options for a category
+    $('<option>').text('General purpose item').appendTo(categorySelector);
+	$('<option/>').text('Electronics').appendTo(categorySelector);
+	$('<option/>').text('Food').appendTo(categorySelector);
+	$('<option/>').text('Household appliance').appendTo(categorySelector);
+	$('<option/>').text('Vehicles').appendTo(categorySelector);
+	$('<option/>').text('Clothes').appendTo(categorySelector);
 
+    
+
+    $('<td/>').attr('colspan','2').append(categorySelector).appendTo(categoryRow);
+     
 
 	$('<td/>').text("Name").appendTo(nameRow);
 	$('<td/>').append(nameInput).appendTo(nameRow);
@@ -46,9 +70,17 @@ function generateTable (argument) {
 	$('<td/>').append(imageURLInput).appendTo(imageURLRow);
     
     $('<td/>').append(submitButton).attr('colspan','2').appendTo(submitRow); 
+
+    
     
 
-	table.append(nameRow).append(descriptionRow).append(priceRow).append(imageURLRow).append(submitRow); 
+	table.append(nameRow).append(descriptionRow)
+	                     .append(priceRow)
+	                     .append(imageURLRow)
+	                     .append(categoryRow)
+	                     .append(submitRow);
+
+
 	tableWrapper.append(table);
 
 

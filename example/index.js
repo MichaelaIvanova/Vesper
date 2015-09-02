f625d5d3dc9f8355127dc3f4aa5f29f517fbb72e
MichaelaIvanova <michaela.av.ivanova@gmail.com>
2015-09-02 18:38:50 +0300

    User log in added

--- example/index.js
+++ example/index.js
@@ -5,13 +5,14 @@ $(function() {
  		$("#register-form").fadeOut(100);
 		$('#register-form-link').removeClass('active');
 		$(this).addClass('active');
-		e.preventDefault();
+		//e.preventDefault();
 	});
 	$('#register-form-link').click(function(e) {
 		$("#register-form").delay(100).fadeIn(100);
  		$("#login-form").fadeOut(100);
 		$('#login-form-link').removeClass('active');
 		$(this).addClass('active');
-		e.preventDefault();
+		//e.preventDefault();
 	});
+
 });
--- example/user-logging.js
+++ example/user-logging.js
@@ -1,13 +1,24 @@
 Parse.initialize("xTAsfMOLmOoOQlLbQkYAWgeIy1ixgHk306RMJFdh", "9Z17vijIoN14L2XXvRM0Uta2AmrCMNsZW03HIG55");
 
 var $signUpButton = $('#register-submit');
-console.log($signUpButton);
 var $signUpFieldUsername = $('#username-register');
 var $signUpFieldPasswordInitial = $('#password-register');
 var $signUpFieldEmail =$('#email');
 var $signUpFieldPasswordConfirmed = $('#confirm-password');
 
+var $signInButton = $('#login-submit');
+var $signInFieldUsername =$('#username-log-in');
+var $signInFieldPassword = $('#password-log-in');
 
+function saveCurrentUserSession(username){
+    if(typeof username === 'undefined' || username === null){
+        throw new Error('Incorrect username!');
+    }
+
+    sessionStorage.setItem('sessionUser', username);
+}
+
+//////// SIGN UP /////////////////////////
 $signUpButton.on('click', function(event) {
     alert('Button clicked');
 
@@ -25,11 +36,14 @@ $signUpButton.on('click', function(event) {
         user.set("password", $signUpFieldPasswordInitial.val());
         user.set("dataStored", []);
         user.set("email", $signUpFieldEmail.val());
+
         user.signUp(null, {
             success: function (user) {
                 alert('User just signed up');
-                /*saveCurrentUserSession($signUpFieldUsername.val());
-                //$nameTitle.html('Logged in as ' + $signUpFieldUsername.val());
+
+                saveCurrentUserSession($signUpFieldUsername.val());
+
+               /* //$nameTitle.html('Logged in as ' + $signUpFieldUsername.val());
                 $invalidPassword.detach();
                 //$container.prepend($logOut);
                 //$container.prepend($nameTitle);
@@ -47,3 +61,40 @@ $signUpButton.on('click', function(event) {
     }
 });
 
+///////// SIGN IN ///////////////////
+
+$signInButton.on('click', function(ev) {
+
+    event.preventDefault();
+    var $this = $(this);
+
+    var $formSignin = $('.form-sign-in');
+    var loggedInUser = Parse.User.current();
+    Parse.User.logOut();
+
+    loggedInUser =  Parse.User.current();
+    console.log(loggedInUser);
+
+    if(!loggedInUser){
+        Parse.User.logIn($signInFieldUsername.val(), $signInFieldPassword.val(), {
+            success: function(user) {
+                saveCurrentUserSession($signInFieldUsername.val());
+                //displayData();
+
+                //console.log(user.get('username'));
+                var collection = user.get('dataStored');
+
+                console.log(collection);
+
+            },
+            error: function(user, error) {
+                alert('NO SUCH USER!');
+
+
+            }
+        });
+    } else {
+        console.log(loggedInUser.get("username"));
+    }
+
+});
